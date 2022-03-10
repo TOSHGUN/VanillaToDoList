de17914aff3031e982dca9b03b5d46768a44fdf5
Anthony <anthonybudin-18@yandex.ru>
2022-03-10 11:10:00 +0300

    made appending to the list

--- app.js
+++ app.js
@@ -19,5 +19,15 @@ function addTodo(event){
     newTodo.classList.add('todo-item');
     todoDiv.appendChild(newTodo);
     // CHECK MARK BUTTON
-
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i  class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // CHECK MARK BUTTON
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i  class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //APPEND TO LIST
+    todoList.appendChild(todoDiv);
 }
